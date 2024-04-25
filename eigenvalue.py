ae754abfa6cdeeacf6932c846921daf999eb4340
Tom Joshi-Cale <tj294@exeter.ac.uk>
2024-04-25 13:23:09 +0000

    Working code

--- eigenvalue.py
+++ eigenvalue.py
@@ -3,24 +3,57 @@ from eigentools import Eigenproblem, CriticalFinder
 import numpy as np
 import argparse
 import matplotlib.pyplot as plt
+from matplotlib import transforms
 from mpi4py import MPI
 import time
-
+from scipy import interpolate, optimize
+import argparse
+import os
 import logging
 
 logger = logging.getLogger(__name__.split(".")[-1])
 
+
+def plot_grid(cf, Ta):
+    fig = plt.figure(figsize=(9, 6))
+    ax = fig.add_subplot(111)
+    yy = cf.parameter_grids[0]
+    zz = cf.parameter_grids[1]
+    grid = cf.evalue_grid.real
+    biggest_val = 2 * np.abs(grid).std()
+    plt.pcolormesh(yy, zz, grid, cmap="RdBu_r", vmin=-biggest_val, vmax=biggest_val)
+    plt.colorbar()
+    plt.xlabel("ky")
+    plt.ylabel("Ra")
+    plt.savefig(f"Ta{args.Ta}_grid.png")
+
+
+parser = argparse.ArgumentParser(description="Critical Finder")
+parser.add_argument(
+    "--Ra", type=str, nargs=2, default=["1e2", "1e4"], help="Limits of Ra Sweep"
+)
+parser.add_argument(
+    "--k", type=str, nargs=2, default=["2", "10"], help="Limits of k Sweep"
+)
+parser.add_argument("--n", type=str, default="50", help="Resolution of Sweep")
+parser.add_argument("--Ta", type=str, default="1e4", help="Taylor number to simulate")
+parser.add_argument(
+    "--overwrite", "-o", action="store_true", help="Overwrite existing grid"
+)
+
+args = parser.parse_args()
+# print(args)
 comm = MPI.COMM_WORLD
 
-Nz = 128
+Nz = 256
 z_basis = de2.Chebyshev("z", Nz, interval=(0, 1))
 d = de2.Domain([z_basis], np.complex128, comm=MPI.COMM_SELF)
 z = z_basis.grid()
 
 pert = de2.EVP(d, ["u", "v", "w", "P", "T", "uz", "vz", "wz", "Tz"], eigenvalue="omega")
-pert.parameters["Rf"] = 1e5
+pert.parameters["Rf"] = 1e7
 pert.parameters["Pr"] = 1
-pert.parameters["Ta"] = 1e4**0.5
+pert.parameters["Ta"] = float(args.Ta) ** 0.5
 theta = np.radians(5)
 pert.parameters["cos_phi"] = np.cos(theta)
 pert.parameters["sin_phi"] = np.sin(theta)
@@ -89,11 +122,15 @@ if comm.rank == 0:
     print("\n### Generating Grid ###\n")
 start = time.time()
 
-direc = "grid"
+direc = f"Ta{args.Ta}_grid"
 
-nRa, nk = 50, 50
-Rapoints = np.linspace(500, 3000, nRa)
-kpoints = np.linspace(2, 10, nk)
+nRa = nk = int(args.n)
+Rapoints = np.linspace(float(args.Ra[0]), float(args.Ra[1]), nRa)
+kpoints = np.linspace(float(args.k[0]), float(args.k[1]), nk)
+
+if args.overwrite:
+    if os.path.isfile(f"{direc}.h5"):
+        os.remove(f"{direc}.h5")
 
 try:
     cf.load_grid(f"{direc}.h5")
@@ -102,6 +139,8 @@ except:
     if comm.rank == 0:
         cf.save_grid(direc)
 
+plot_grid(cf, args.Ta)
+# cf.plot_crit(xlabel="k", ylabel="Ra", cmap="RdBu_r")
 
 end = time.time()
 if comm.rank == 0:
@@ -113,3 +152,70 @@ begin = time.time()
 crit = cf.crit_finder(polish_roots=True, tol=1e-5)
 end = time.time()
 logger.info("critical finding/root polishing time: {:10.5f} sec".format(end - start))
+
+
+if comm.rank == 0:
+    print("crit = {}".format(crit))
+    print("critical wavenumber k = {:10.5f}".format(crit[0]))
+    print("critical Ra = {:10.5f}".format(crit[1]))
+    print("critical freq = {:10.5f}".format(crit[2]))
+
+    mask = np.isfinite(cf.roots)
+    yy_root = cf.parameter_grids[0][0, mask]
+    rroot = cf.roots[mask]
+    root_fn = interpolate.interp1d(
+        yy_root, rroot, kind="cubic"
+    )  # Interpolating over masked roots
+    y_fn = np.linspace(
+        yy_root[0], yy_root[-1], 100
+    )  # Constructing a kx array for plotting
+    #
+    n = 128
+    Ly = 4
+    ky_restrict = []
+
+    # print(yy_root, rroot, n * 2 * np.pi / Ly, yy_root[-1])
+    # Restrict values of kx to those which fit in our box shape Lx
+    while n * 2 * np.pi / Ly < yy_root[-1]:
+        if n * 2 * np.pi / Ly < yy_root[0]:
+            pass
+        else:
+            ky_restrict.append(n * 2 * np.pi / Ly)
+        n += 1
+    print(f"Restricted ky values for Ly = {Ly}:")
+    print(ky_restrict)
+    Ra_restrict = root_fn(ky_restrict)
+
+    # index = np.where(Ra_restrict == np.min(Ra_restrict))[0][0]
+    index = 0
+    #
+    fig = plt.figure(figsize=(9, 6))
+    ax = fig.add_subplot(111)
+    xx = cf.parameter_grids[0]
+    yy = cf.parameter_grids[1]
+    grid = cf.evalue_grid.real
+    biggest_val = 2 * np.abs(grid).std()
+    plt.pcolormesh(xx, yy, grid, cmap="RdBu_r", vmin=-biggest_val, vmax=biggest_val)
+    plt.colorbar()
+    x = cf.parameter_grids[0][0, :]
+    y = cf.roots[:]
+    plt.scatter(x, y)
+    plt.plot(y_fn, root_fn(y_fn), ":")
+    plt.ylim(yy.min(), yy.max())
+    plt.xlim(xx.min(), xx.max())
+    plt.xlabel("ky")
+    plt.ylabel("Ra")
+
+    # plt.title(
+    #     "All ky: Ra_c = {:.2f}, ky_c = {:.3f} \n Restricted ky: Lx = {}, Ra_c = {:.2f}, ky_c = {:.3f} \n ".format(
+    #         crit[1], crit[0], Ly, Ra_restrict[index], ky_restrict[index]
+    #     )
+    # )
+    plt.title(
+        "All ky: Ra_c = {:.2f}, ky_c = {:.3f} \n ".format(
+            crit[1],
+            crit[0],
+        )
+    )
+    plt.tight_layout()
+    plt.savefig("crit_finder.png")
